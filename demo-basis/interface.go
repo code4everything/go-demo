5a30978b4092af61594c620c562c9ee5fe0c9bfc
pantao <tao@zhazhapan.com>
2019-06-06 21:57:27 +0800

    接口的嵌套与type-switch

--- demo-basis/interface.go
+++ demo-basis/interface.go
@@ -5,6 +5,29 @@ import "fmt"
 func main() {
 	var l learner = employer{"trump"}
 	l.learn()
+	// 转型（实际返回两个参数，第二个为是否转换成功的布尔值）
+	var e = l.(employer)
+	fmt.Println(e.name)
+
+	fmt.Println(typeOf(true))
+}
+
+// type switch
+// 空接口可以看成所有类型的基类
+func typeOf(item interface{}) (t string) {
+	switch item.(type) {
+	case int, int8, int16, int32, int64:
+		t = "integer"
+	case float32, float64:
+		t = "float"
+	case bool:
+		t = "boolean"
+	case string:
+		t = "string"
+	default:
+		t = "unknown"
+	}
+	return
 }
 
 type learner interface {
@@ -17,4 +40,16 @@ type employer struct {
 
 func (e employer) learn() {
 	fmt.Println(e.name, "is learning")
+}
+
+// 接口的继承
+type locker interface {
+	lock()
+	unlock()
+}
+
+type file interface {
+	locker
+	read()
+	write()
 }
