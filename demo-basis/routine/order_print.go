ce378b15f2a904e478611478d537d0d8a3b31556
pantao <tao@zhazhapan.com>
2019-06-21 22:03:58 +0800

    feat: routine synchronize

--- demo-basis/routine/order_print.go
+++ demo-basis/routine/order_print.go
@@ -2,26 +2,31 @@ package main
 
 import "fmt"
 
+var arr = make([]int, 100)
+
 func main() {
-	ch1 := make(chan int, 1)
+	ch1 := make(chan int)
 	ch2 := make(chan int)
 	go routine1(ch1)
 	go routine2(ch1, ch2)
 	// waiting for complete
 	<-ch2
+	for i := 0; i < 100; i++ {
+		fmt.Println(arr[i])
+	}
 }
 
 func routine1(ch chan int) {
 	for i := 1; i < 100; i += 2 {
+		arr[i-1] = i
 		ch <- i
-		fmt.Println("one:", i)
 	}
 	close(ch)
 }
 
 func routine2(ch1, ch2 chan int) {
 	for v := range ch1 {
-		fmt.Println("two:", v+1)
+		arr[v] = v + 1
 	}
 	// completed flag
 	ch2 <- 1
