14cb8e47823e989b2030cfd690017c9a76ba446d
pantao <easepan@qq.com>
2019-06-27 15:49:53 +0800

    feat: do leetcode '二叉树的最小深度'

--- demo-algorithm/list/solution.go
+++ demo-algorithm/list/solution.go
@@ -4,6 +4,27 @@ import (
 	. "../leetcode"
 )
 
+// LeetCode(id=111,title=二叉树的最小深度,difficulty=easy)
+func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	if root.Left == nil && root.Right == nil {
+		return 1
+	}
+	var min int
+	if root.Left != nil {
+		min = minDepth(root.Left)
+	}
+	if root.Right != nil {
+		tmp := minDepth(root.Right)
+		if min == 0 || tmp < min {
+			min = tmp
+		}
+	}
+	return 1 + min
+}
+
 var levels = make([][]int, 0)
 
 // LeetCode(id=100,title=二叉树的层次遍历 II,difficulty=easy)
