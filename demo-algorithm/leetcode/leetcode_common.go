f44cdbc7e03fdd2cd9298d0b1b79b54f71bdf926
pantao <easepan@qq.com>
2019-06-27 11:52:31 +0800

    feat: do leetcode '相同的树'

--- demo-algorithm/leetcode/leetcode_common.go
+++ demo-algorithm/leetcode/leetcode_common.go
@@ -1,6 +1,14 @@
 package leetcode
 
+// 单链表
 type ListNode struct {
 	Val  int
 	Next *ListNode
+}
+
+// 二叉树
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
 }
--- demo-algorithm/list/solution.go
+++ demo-algorithm/list/solution.go
@@ -2,6 +2,20 @@ package list
 
 import . "../leetcode"
 
+// LeetCode(id=100,title=相同的树,difficulty=easy)
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p == nil || q == nil {
+		return false
+	}
+	if p.Val == q.Val {
+		return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+	}
+	return false
+}
+
 // LeetCode(id=83,title=删除排序链表中的重复元素,difficulty=easy)
 func deleteDuplicates(head *ListNode) *ListNode {
 	if head == nil || head.Next == nil {
