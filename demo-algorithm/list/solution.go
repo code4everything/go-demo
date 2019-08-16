7e57b2620a8272af1722fe0f8abb43586c291ef2
pantao <easepan@qq.com>
2019-08-16 18:04:49 +0800

    feat: solve problem 671 on leetcode

--- demo-algorithm/list/solution.go
+++ demo-algorithm/list/solution.go
@@ -2,8 +2,38 @@ package list
 
 import (
 	. "../leetcode"
+	"sort"
 	"strconv"
 )
+
+// LeetCode(id=671,title=二叉树中第二小的节点,difficulty=easy)
+func findSecondMinimumValue(root *TreeNode) int {
+	if root == nil || root.Left == nil || root.Right == nil {
+		return -1
+	}
+	vals := []int{root.Val, root.Left.Val, root.Right.Val}
+	var getNodeVals func(node *TreeNode)
+	getNodeVals = func(node *TreeNode) {
+		if node.Left != nil && node.Right != nil {
+			vals = append(vals, node.Left.Val)
+			vals = append(vals, node.Right.Val)
+			getNodeVals(node.Left)
+			getNodeVals(node.Right)
+		}
+	}
+	getNodeVals(root.Left)
+	getNodeVals(root.Right)
+	sort.Ints(vals)
+	if vals[0] == vals[len(vals)-1] {
+		return -1
+	}
+	for _, v := range vals {
+		if v > vals[0] {
+			return v
+		}
+	}
+	return -1
+}
 
 // LeetCode(id=669,title=修剪二叉搜索树,difficulty=easy)
 func trimBST(root *TreeNode, L int, R int) *TreeNode {
