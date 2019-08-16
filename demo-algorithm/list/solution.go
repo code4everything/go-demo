b631f9c8642522eaaec8c9d80c932a4bab8f4324
pantao <easepan@qq.com>
2019-08-16 17:04:13 +0800

    feat: solve problem 669 on leetcode

--- demo-algorithm/list/solution.go
+++ demo-algorithm/list/solution.go
@@ -5,6 +5,22 @@ import (
 	"strconv"
 )
 
+// LeetCode(id=669,title=修剪二叉搜索树,difficulty=easy)
+func trimBST(root *TreeNode, L int, R int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val > R {
+		return trimBST(root.Left, L, R)
+	}
+	if root.Val < L {
+		return trimBST(root.Right, L, R)
+	}
+	root.Left = trimBST(root.Left, L, R)
+	root.Right = trimBST(root.Right, L, R)
+	return root
+}
+
 var bst []int
 
 // LeetCode(id=653,title=两数之和 IV - 输入 BST,difficulty=easy)
