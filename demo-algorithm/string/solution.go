d8c0e293fd973d5816129501cb7bc57cfe01c3f7
pantao <easepan@qq.com>
2019-09-22 23:02:47 +0800

    feat: solve problem 796 on leetcode

--- demo-algorithm/string/solution.go
+++ demo-algorithm/string/solution.go
@@ -6,6 +6,32 @@ import (
 	"strings"
 )
 
+// LeetCode(id=796,title=旋转字符串,difficulty=easy)
+func rotateString(A string, B string) bool {
+    lenA := len(A)
+        lenB := len(B)
+        if lenA != lenB{
+            return false
+        }
+        if lenA == 0{
+            return true
+        }
+        for i:= 0; i < lenA; i++{
+            temp, j := i, 0
+            for j < lenB{
+                if A[temp%lenA] != B[j]{
+                    break
+                }
+                temp++
+                j++
+            }
+            if j == lenB{
+                return true
+            }
+        }
+    return false
+}
+
 // LeetCode(id=748,title=最短完整词,difficulty=easy)
 func shortestCompletingWord(licensePlate string, words []string) string {
 	licensePlate = strings.ToLower(licensePlate)
